100c628790189c6fd8bd8e7d4cba49e12562a72b
Katy Deng <deng2017.xq@gmail.com>
2018-05-11 12:19:54 -0700

    init commit;

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express'); 
+
+const PORT = process.env.PORT || 9000;
+
+const app = express(); 
+
+//using .get  creating one end point 
+
+app.get('/', function (req, res){
+  res.send ('<h1>Hello World!</h1>')
+})
+
+app.listen( PORT, function(){
+  console.log('Express Server running at localhost! ' + PORT);
+} ).on('error', (err)=>{
+  console.log('Server Error', err.message); 
+  console.log('Do you already have a server running on PORT: ' + PORT +'?')
+}); 
