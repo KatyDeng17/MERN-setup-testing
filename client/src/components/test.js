2f263721c4d239ac670fcea58a3ad33abffef78e
Katy Deng <deng2017.xq@gmail.com>
2018-05-12 03:13:29 -0700

    pushing client side ffiles'

--- client/src/components/test.js
+++ client/src/components/test.js
@@ -4,14 +4,35 @@ import axios from 'axios';
 class Test extends Component{
   componentDidMount(){
     this.getUserData();
+    this.getArticle();
+    this. sendData();
   }
   //requesting data from server and console.log() the data back; 
+///sending data from front end to back end; 
+  async sendData(){
+    //demo data:
+    const dataToSend = {
+      somthing : 'here is something', 
+      anothering: 'here is another thing', 
+      
+    };
+    const response = await axios.post('/api/send-data', dataToSend);
+    console.log('Data Sent to server:', response);
+  }
+
+// requesting data from front to backend; 
   async getUserData(){
-    const response = await axios.get('http://localhost:9000/user-data');
+    const response = await axios.get('/api/user-data');
     console.log('user Data Response:', response); 
   }
+
+
+  async getArticle(){
+    const response = await axios.get('/api/get-article'); 
+    console.log('Article data response', response);
+  }
   render(){
-    return <h1> Test Component</h1> 
+    return <h1> Test</h1> 
   }
 }
 
--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const express = require('express'); 
 const cors = require('cors');
-const PORT = process.env.PORT || 9000;
+const { resolve } = require('path');
+const PORT = process.env.PORT || 8000;
 
 
 const app = express(); 
 
+//when doing get
+app.use(cors()); 
+//when doing POST
+app.use(express.json());
 
-app.use(cors());
+//serve everything inside the client folder for React
+app.use(express.static(resolve(__dirname, 'client','dist')));
 
 
 
-//using .get  creating one end point 
+//using .get  creating end points 
 
-app.get('/user-data',(req, res)=>{
+//MAKING POST request///////////////
+//receiving data from front end; and send it back to front end; 
+app.post('/api/send-data',(req,res)=>{
+  console.log("data sent: ", req.body);
+  res.send({success: true, mirror: req.body }); //for testing after getting the data from client then send right back to the client? 
+})
+
+//front end requesting data from back end! 
+//MAKING get request /////////////// 
+app.get('/api/user-data',(req, res)=>{
+  //demo data in back-end!
   const user ={
    name : 'Jim Bob',
    email: 'jimhebob@gmail.com'
@@ -20,9 +36,29 @@ app.get('/user-data',(req, res)=>{
   res.send(user);
 });
 
+
+app.get('/api/get-article', (req,res)=>{
+  //demo data in back-end!
+  const article = {
+    title: 'Some Awesome Article',
+    author: 'John Walker',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, placeat nesciunt. At quam nulla magni error minus, eius nostrum, nemo voluptatibus laborum id nam, ratione dolore consequuntur? Non, soluta quia.'
+  }
+   res.send(article);
+});
+
+
+
+
 app.get('/', function (req, res){
-  res.send ('<h1>somthing</h1>')
+  res.send ('<h1>something is great!</h1>')
 })
+
+///last end point 
+app.get('*',(req, res)=>{
+  res.sendFile(resolve(__dirname, 'client', 'dist', 'index.html'));
+})
+
 
 app.listen( PORT, function(){
   console.log('Express Server running at localhost! ' + PORT);
